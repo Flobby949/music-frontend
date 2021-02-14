a045d818d6b16edc8b56b51e37c18179b8e5417b
Flobby <2541226493@qq.com>
2021-02-14 22:18:19 +0800

    项目初始化

---
+++ src/views/playlist/list.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h2>歌单管理</h2>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
---
+++ src/views/swiper/list.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h2>轮播图管理</h2>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
